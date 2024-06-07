865e7aeece04dd34958ae661f1438a0c1c973f49
Macmilan24 <cpt.macmilan24@gmail.com>
2024-06-07 18:10:07 +0300

    begin on product

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,32 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export default function Card({ cardInfo }) {
+import { useState } from "react";
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const [hovered, setHovered] = useState[false];
+
+const handleEnter = () => {
+  setHovered(true);
+};
+
+const handleLeave = () => {
+  setHovered(false);
+};
+
+export default function Card({ cardInfo, className }) {
   return (
-    <div className="flex cursor-pointer basis-1/3 mb-5 pr-5">
+    <div
+      className={`flex cursor-pointer basis-1/3 mb-5 pr-5 ${className}`}
+      onMouseEnter={handleEnter}
+      onMouseLeave={handleLeave}
+    >
       <div className=" text-orange-400 text-2xl">{cardInfo.icon}</div>
-      <div className="pl-2">
-        <h3 className=" text-sm font-light mb-1">{cardInfo.title}</h3>
+      <div className="pl-4">
+        <h3
+          className={`text-sm font-light mb-1 ${
+            hovered ? "text-orange-400" : ""
+          }`}
+        >
+          {cardInfo.title}
+        </h3>
         <p className=" text-xs text-gray-500">{cardInfo.des}</p>
       </div>
     </div>
--- src/container/dropDown/Product.js
+++ src/container/dropDown/Product.js
@@ -1,11 +1,45 @@
 import Card from "../../components/card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Product({ onHover, onLeave }) {
   return (
     <div
       onMouseEnter={onHover}
       onMouseLeave={onLeave}
-      className="w-full px-28 py-8 flex flex-wrap absolute z-10 bg-white"
-    ></div>
+      className="w-full px-28 py-2 flex flex-wrap absolute z-10 bg-white pr-4"
+    >
+      <div className=" basis-1/4 h-30 flex flex-col pr-8 items-start">
+        <div className=" flex flex-col w-full">
+          <h3 className=" uppercase ml-2 text-base font-medium leading-3 mb-9">
+            Platforms
+          </h3>
+          <hr className=" mb-9 border-y-yellow-400" />
+        </div>
+        <Card
+          className="mb-14"
+          cardInfo={{
+            title: "Employee Engagement",
+            des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+            icon: <FontAwesomeIcon icon="briefcase" />,
+          }}
+        />
+        <Card
+          className="mb-14"
+          cardInfo={{
+            title: "Employee Engagement",
+            des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+            icon: <FontAwesomeIcon icon="briefcase" />,
+          }}
+        />
+        <Card
+          className="mb-14"
+          cardInfo={{
+            title: "Employee Engagement",
+            des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+            icon: <FontAwesomeIcon icon="briefcase" />,
+          }}
+        />
+      </div>
+    </div>
   );
 }
