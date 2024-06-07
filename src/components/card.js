2cf39f64608377dd723891a9fd574210dffe9e3c
Macmilan24 <cpt.macmilan24@gmail.com>
2024-06-07 21:07:10 +0300

    finished the navbar

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,6 @@
 export default function Card({ cardInfo, className }) {
   return (
-    <div className={`flex cursor-pointer basis-1/3 mb-5 pr-5 ${className}`}>
+    <div className={`flex cursor-pointer basis-1/3 mb-5  ${className}`}>
       <div className=" text-orange-400 text-2xl">{cardInfo.icon}</div>
       <div className="pl-4">
         <h3 className="text-sm font-light mb-1 hover:text-orange-400">
--- src/container/dropDown/Product.js
+++ src/container/dropDown/Product.js
@@ -1,44 +1,136 @@
 import Card from "../../components/card";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const platInfo = [
+  {
+    title: "Citizen Engagement",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="users" />,
+  },
+  {
+    title: "European Projects",
+    des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+    icon: <FontAwesomeIcon icon="euro-sign" />,
+  },
+  {
+    title: "Social housing",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="home" />,
+  },
+];
+const toolInfo = [
+  {
+    title: "Citizen Engagement",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="users" />,
+  },
+  {
+    title: "European Projects",
+    des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+    icon: <FontAwesomeIcon icon="euro-sign" />,
+  },
+  {
+    title: "Social housing",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="home" />,
+  },
+  {
+    title: "Employee Engagement",
+    des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+    icon: <FontAwesomeIcon icon="briefcase" />,
+  },
+  {
+    title: "Public transportation",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="train" />,
+  },
+  {
+    title: "Open innovation",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="lightbulb" />,
+  },
+];
+const featureInfo = [
+  {
+    title: "Citizen Engagement",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="users" />,
+  },
+  {
+    title: "European Projects",
+    des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+    icon: <FontAwesomeIcon icon="euro-sign" />,
+  },
+  {
+    title: "Social housing",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="home" />,
+  },
+  {
+    title: "Employee Engagement",
+    des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
+    icon: <FontAwesomeIcon icon="briefcase" />,
+  },
+  {
+    title: "Public transportation",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="train" />,
+  },
+  {
+    title: "Open innovation",
+    des: "Empowerinig community voices for impactful local changes through increased particpation",
+    icon: <FontAwesomeIcon icon="lightbulb" />,
+  },
+];
+
 export default function Product({ onHover, onLeave }) {
   return (
     <div
       onMouseEnter={onHover}
       onMouseLeave={onLeave}
-      className="w-full px-28 py-2 flex flex-wrap absolute z-10 bg-white pr-4"
+      className="w-full px-28 py-2 flex absolute z-10 bg-white"
     >
-      <div className=" basis-1/4 h-30 flex flex-col pr-8 items-start">
+      <div className=" basis-1/4 h-30 flex flex-col items-start pr-8">
         <div className=" flex flex-col w-full">
           <h3 className=" uppercase ml-2 text-base font-medium leading-3 mb-9">
             Platforms
           </h3>
-          <hr className=" mb-9 border-y-yellow-400" />
+          <hr className=" mb-9 border-y-yellow-400 pr-4" />
         </div>
-        <Card
-          className="mb-14"
-          cardInfo={{
-            title: "Employee Engagement",
-            des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
-            icon: <FontAwesomeIcon icon="briefcase" />,
-          }}
-        />
-        <Card
-          className="mb-14"
-          cardInfo={{
-            title: "Employee Engagement",
-            des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
-            icon: <FontAwesomeIcon icon="briefcase" />,
-          }}
-        />
-        <Card
-          className="mb-14"
-          cardInfo={{
-            title: "Employee Engagement",
-            des: "Creating a vibrant and innovative work Culture by fostering collaborating among employees",
-            icon: <FontAwesomeIcon icon="briefcase" />,
-          }}
-        />
+        {platInfo.map((card) => (
+          <Card cardInfo={card} className="mb-11" />
+        ))}
+      </div>
+
+      <div className=" h-50 w-px bg-yellow-200  mr-8"></div>
+
+      <div className=" basis-3/4 flex flex-col">
+        <div className=" h-30 flex flex-col items-start pr-8">
+          <div className=" flex flex-col w-full">
+            <h3 className=" uppercase ml-2 text-base font-medium leading-3 mb-9">
+              Engagement Tools
+            </h3>
+            <hr className=" mb-9 border-y-yellow-400 pr-4" />
+          </div>
+          <div className="flex flex-wrap">
+            {toolInfo.map((card) => (
+              <Card cardInfo={card} />
+            ))}
+          </div>
+        </div>
+        <div className=" h-30 flex flex-col items-start pr-8">
+          <div className=" flex flex-col w-full">
+            <h3 className=" uppercase ml-2 text-base font-medium leading-3 mb-9">
+              Engagement Features
+            </h3>
+            <hr className=" mb-9 border-y-yellow-400 pr-4" />
+          </div>
+          <div className="flex flex-wrap">
+            {featureInfo.map((card) => (
+              <Card cardInfo={card} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
