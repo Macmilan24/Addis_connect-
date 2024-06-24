4b866bca3f8c25725e446fc9d636b77b35e6ca7c
Macmilan24 <cpt.macmilan24@gmail.com>
2024-06-24 11:42:38 +0300

    finished on the landing page

--- src/container/WelcomeSections/WelcomeSections.js
+++ src/container/WelcomeSections/WelcomeSections.js
@@ -6,7 +6,7 @@ const WelcomeSection = () => {
       className="relative h-screen bg-cover bg-center text-center p-10 bg-gray-100 z-0"
       style={{ backgroundImage: `url(/img/logo.jpg)` }}
     >
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-20"></div>
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
         <h1 className="text-3xl md:text-5xl font-bold">
           Welcome to CitizenConnect
